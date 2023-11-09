dfcc98ecd4c2cbfbcc232c6007cd5a8fe2f828f6
Azizul Islam <imazizul@gmail.com>
2023-11-09 11:48:55 +0600

    added changed

--- k6test.js
+++
@@ -1,9 +0,0 @@
-import http from 'k6/http';
-import { check } from 'k6';
-
-export default function () {
-  const res = http.get('https://httpbin.test.k6.io');
-  check(res, {
-    'response code was 200': (res) => res.status !== 200,
-  });
-}
